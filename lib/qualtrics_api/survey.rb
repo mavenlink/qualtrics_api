58559c589f3c38cd70cfda6527cd423f1a97d8f2
John Flores <jflores@mavenlink.com>
2018-11-16 16:52:48 -0800

    Update survey attributes

--- lib/qualtrics_api/survey.rb
+++ lib/qualtrics_api/survey.rb
@@ -4,8 +4,20 @@ module QualtricsAPI
       attribute :id, String
       attribute :name, String
       attribute :owner_id, String
-      attribute :last_modified, String
+      attribute :organization_id, String
       attribute :is_active, Boolean
+      attribute :creation_date, String
+      attribute :last_modified_date, String
+      attribute :last_modified, String
+      attribute :expiration, Json
+      attribute :questions, Json
+      attribute :export_column_map, Json
+      attribute :blocks, Json
+      attribute :flow, Array[Json]
+      attribute :embedded_data, Array[Json]
+      attribute :comments, Json
+      attribute :loop_and_merge, Json
+      attribute :response_counts, Json
     end
 
     def export_responses(export_options = {})
@@ -19,8 +31,20 @@ module QualtricsAPI
         :id => "id",
         :name => "name",
         :owner_id => "ownerId",
+        :organization_id => "organizationId",
+        :is_active => "isActive",
+        :creation_date => "creationDate",
+        :last_modified_date => "lastModifiedDate",
         :last_modified => "lastModified",
-        :is_active => "isActive"
+        :expiration => "expiration",
+        :questions => "questions",
+        :export_column_map => "exportColumnMap",
+        :blocks => "blocks",
+        :flow => "flow",
+        :embedded_data => "embeddedData",
+        :comments => "comments",
+        :loop_and_merge => "loopAndMerge",
+        :response_counts => "responseCounts"
       }
     end
   end
