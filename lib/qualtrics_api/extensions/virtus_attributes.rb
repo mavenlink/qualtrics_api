93a66821478509eb85e65458c44c811fd692ac2c
John Flores <jflores@mavenlink.com>
2018-11-19 13:31:31 -0800

    Only underscore non-id hash keys
    Keys that are all uppercase are ids referenced elsewhere within the response, so keeping them the same as their values elsewhere helps access hashes where they are keys.

--- lib/qualtrics_api/extensions/virtus_attributes.rb
+++ lib/qualtrics_api/extensions/virtus_attributes.rb
@@ -3,9 +3,13 @@ module QualtricsAPI
     module VirtusAttributes
       class Json < Virtus::Attribute
         def coerce(value)
+          return value unless value.present?
+
           value = JSON.parse(value) unless value.is_a? ::Hash
 
-          value.deep_transform_keys { |key| key.underscore.to_sym }
+          value.deep_transform_keys do |key|
+            key.upcase == key ? key.to_sym : key.underscore.to_sym
+          end.with_indifferent_access
         end
       end
     end
