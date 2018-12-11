17bf11539dfe2e527210079ab9e36d78af370f7c
John Flores <jflores@mavenlink.com>
2018-12-11 15:19:03 -0800

    Add missing user attributes
    Co-authored-by: Alejandro Rincon <arincon@mavenlink.com>

--- lib/qualtrics_api/user.rb
+++ lib/qualtrics_api/user.rb
@@ -9,6 +9,17 @@ module QualtricsAPI
       attribute :user_type, String
       attribute :email, String
       attribute :account_status, String
+      attribute :organization_id, String
+      attribute :language, String
+      attribute :unsubscribed, Boolean
+      attribute :account_creation_date, String
+      attribute :account_expiration_date, String
+      attribute :password_last_changed_date, String
+      attribute :password_expiration_date, String
+      attribute :last_login_date, String
+      attribute :timezone, String
+      attribute :response_counts, Json
+      attribute :permissions, Json
     end
 
     def messages(options = {})
@@ -26,7 +37,18 @@ module QualtricsAPI
         :last_name => "lastName",
         :user_type => "userType",
         :email => "email",
-        :account_status => "active"
+        :account_status => "accountStatus",
+        :organization_id  => "organizationId",
+        :language  => "language",
+        :unsubscribed => "unsubscribed",
+        :account_creation_date => "accountCreationDate",
+        :account_expiration_date => "accountExpirationDate",
+        :password_last_changed_date => "passwordLastChangedDate",
+        :password_expiration_date => "passwordExpirationDate",
+        :last_login_date => "lastLoginDate",
+        :timezone => "timezone",
+        :response_counts => "responseCounts",
+        :permissions => "permissions"
       }
     end
   end
