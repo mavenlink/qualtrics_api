cec3d63f28981b2fbb35daf4cedab0634d04265c
Alejandro Rincon <arincon@mavenlink.com>
2019-02-01 09:23:20 -0800

    Add reload for directory contact import
    Co-authored-by: John Flores <jflores@mavenlink.com>

--- lib/qualtrics_api/directory_contact_import.rb
+++ lib/qualtrics_api/directory_contact_import.rb
@@ -2,11 +2,19 @@ module QualtricsAPI
   class DirectoryContactImport < BaseModel
     values do
       attribute :id, String
+      attribute :directory_id, String
+      attribute :mailing_list_id, String
       attribute :percent_complete, Float
       attribute :contacts, Json
       attribute :tracking, Json
       attribute :status, String
       attribute :transactions, Json
+    end
+
+    def reload
+      response = QualtricsAPI.connection(self).get(endpoint).body["result"]
+
+      QualtricsAPI::DirectoryContactImport.new(response.merge(id: id, directory_id: directory_id, mailing_list_id: mailing_list_id)).propagate_connection(self)
     end
 
     private
@@ -20,6 +28,10 @@ module QualtricsAPI
         status: "status",
         transactions: "transactions"
       }
+    end
+
+    def endpoint
+      "directories/#{directory_id}/mailinglists/#{mailing_list_id}/transactioncontacts/#{id}"
     end
   end
 end
